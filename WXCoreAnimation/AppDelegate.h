27c91b27bf1af4316435402b9fe6685480e7818c
wuxinIOS <wuxinIOS@163.com>
2017-07-20 10:22:07 +0800

    Initial Commit

---
+++ WXCoreAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXCoreAnimation
+//
+//  Created by BlackEr Gray on 17/7/20.
+//  Copyright © 2017年 BlackEr Gray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
